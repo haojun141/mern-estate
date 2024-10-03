31cece4b499dc4221892e505aef5d1897f9ee7b8
haojun14 <haojun0114@outlook.com>
2024-10-03 13:03:43 +0800

    added signout functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { signup, signin, google } from "../controllers/auth.controller.js"; // Ensure correct path
+import {
+  signup,
+  signin,
+  google,
+  signout,
+} from "../controllers/auth.controller.js"; // Ensure correct path
 const router = express.Router();
 
 // Define the signup route
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/google", google);
+router.get("/signout", signout);
 export default router;
