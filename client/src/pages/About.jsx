8d9aabd9123f34fab40f5b9c7b7272794a72d47e
haojun14 <haojun0114@outlook.com>
2024-10-14 22:12:45 +0800

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,29 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Haojun Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Haojun Estate is a trusted real estate agency that helps clients find
+        their dream homes or investment properties. We focus on providing
+        high-quality service in finding, buying, selling, or renting properties
+        in key locations.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our goal is to deliver unmatched expertise, ensuring that our clients
+        make well-informed decisions in the real estate market. We specialize in
+        a range of property types from residential to commercial spaces,
+        tailoring solutions to fit individual needs.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Whether you're a first-time homebuyer or a seasoned investor, our team
+        is dedicated to guiding you through every step of the process. Our
+        commitment is to provide excellent support and ensure a smooth,
+        stress-free experience for all our clients.
+      </p>
+    </div>
+  );
 }
