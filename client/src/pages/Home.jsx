4eab87a7f2aba7df82d3f5fd690ce4657d87bcba
haojun14 <haojun0114@outlook.com>
2024-10-15 11:06:08 +0800

    edit on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,24 +1,23 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css/bundle";
 import { Navigation } from "swiper/modules";
 import SwiperCore from "swiper";
+import "swiper/css/bundle";
 import ListingItem from "../components/ListingItem";
 
 export default function Home() {
-  const [offerListings, setOfferlistings] = useState([]);
+  const [offerListings, setOfferListings] = useState([]);
   const [saleListings, setSaleListings] = useState([]);
   const [rentListings, setRentListings] = useState([]);
   SwiperCore.use([Navigation]);
-  console.log(saleListings);
-
+  console.log(offerListings);
   useEffect(() => {
     const fetchOfferListings = async () => {
       try {
-        const res = await fetch(`/api/listing/get?offer=true&limit=4`);
+        const res = await fetch("/api/listing/get?offer=true&limit=4");
         const data = await res.json();
-        setOfferlistings(data);
+        setOfferListings(data);
         fetchRentListings();
       } catch (error) {
         console.log(error);
@@ -26,7 +25,7 @@ export default function Home() {
     };
     const fetchRentListings = async () => {
       try {
-        const res = await fetch(`/api/listing/get?type=rent&limit=4`);
+        const res = await fetch("/api/listing/get?type=rent&limit=4");
         const data = await res.json();
         setRentListings(data);
         fetchSaleListings();
@@ -34,13 +33,14 @@ export default function Home() {
         console.log(error);
       }
     };
+
     const fetchSaleListings = async () => {
       try {
-        const res = await fetch(`/api/listing/get?type=sale&limit=4`);
+        const res = await fetch("/api/listing/get?type=sale&limit=4");
         const data = await res.json();
         setSaleListings(data);
       } catch (error) {
-        console.log(error);
+        log(error);
       }
     };
     fetchOfferListings();
@@ -50,7 +50,8 @@ export default function Home() {
       {/* top */}
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
-          Find your next <span className="text-slate-500">Perfect</span> <br />
+          Find your next <span className="text-slate-500">perfect</span>
+          <br />
           place with ease
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
@@ -66,12 +67,13 @@ export default function Home() {
           Let's get started...
         </Link>
       </div>
-      {/* swipper */}
+
+      {/* swiper */}
       <Swiper navigation>
         {offerListings &&
           offerListings.length > 0 &&
           offerListings.map((listing) => (
-            <SwiperSlide key={listing._id}>
+            <SwiperSlide>
               <div
                 style={{
                   background: `url(${listing.imageUrls[0]}) center no-repeat`,
@@ -84,7 +86,8 @@ export default function Home() {
           ))}
       </Swiper>
 
-      {/* listing result for offer sale and rent */}
+      {/* listing results for offer, sale and rent */}
+
       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
         {offerListings && offerListings.length > 0 && (
           <div className="">
@@ -93,8 +96,8 @@ export default function Home() {
                 Recent offers
               </h2>
               <Link
-                to={"/search?offer=true"}
                 className="text-sm text-blue-800 hover:underline"
+                to={"/search?offer=true"}
               >
                 Show more offers
               </Link>
@@ -113,8 +116,8 @@ export default function Home() {
                 Recent places for rent
               </h2>
               <Link
-                to={"/search?type=rent"}
                 className="text-sm text-blue-800 hover:underline"
+                to={"/search?type=rent"}
               >
                 Show more places for rent
               </Link>
@@ -130,11 +133,11 @@ export default function Home() {
           <div className="">
             <div className="my-3">
               <h2 className="text-2xl font-semibold text-slate-600">
-                Recent places for sales
+                Recent places for sale
               </h2>
               <Link
-                to={"/search?type=sale"}
                 className="text-sm text-blue-800 hover:underline"
+                to={"/search?type=sale"}
               >
                 Show more places for sale
               </Link>
