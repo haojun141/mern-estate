81ea4fa197814920669c9c4efe901a7484726230
haojun14 <haojun0114@outlook.com>
2024-10-15 11:10:18 +0800

    edit on signup

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import OAuth from "../components/OAuth";
 
 export default function SignUp() {
@@ -9,22 +8,27 @@ export default function SignUp() {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
   };
   const handleSubmit = async (e) => {
-    e.preventDefault(); //prevent refreshing the page after submitting
+    e.preventDefault();
     try {
       setLoading(true);
       const res = await fetch("/api/auth/signup", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        setError(data.message);
         setLoading(false);
+        setError(data.message);
         return;
       }
       setLoading(false);
@@ -35,10 +39,9 @@ export default function SignUp() {
       setError(error.message);
     }
   };
-
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-center text-3xl font-semibold my-7">Sign Up</h1>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
@@ -61,18 +64,19 @@ export default function SignUp() {
           id="password"
           onChange={handleChange}
         />
+
         <button
           disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
-          {loading ? "Loading.." : "Sign Up"}
+          {loading ? "Loading..." : "Sign Up"}
         </button>
         <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         <Link to={"/sign-in"}>
-          <span className="text-blue-700">Sign In</span>
+          <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
       {error && <p className="text-red-500 mt-5">{error}</p>}
