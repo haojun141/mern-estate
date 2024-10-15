e5547f092f96379d97e67e8d3f9d687b2e5adc04
haojun14 <haojun0114@outlook.com>
2024-10-15 10:54:34 +0800

    edit on auth model

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,15 +1,16 @@
 import express from "express";
 import {
-  signup,
-  signin,
   google,
   signout,
-} from "../controllers/auth.controller.js"; // Ensure correct path
+  signin,
+  signup,
+} from "../controllers/auth.controller.js";
+
 const router = express.Router();
 
-// Define the signup route
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/google", google);
 router.get("/signout", signout);
+
 export default router;
